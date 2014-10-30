caad38b5541b763cd868690a300e72cac77efe2c
David Vieser <david@vieser.org>
2014-10-30 12:54:42 -0500

    javascript changes

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,19 @@ document.createElement("section");
 
 $( "#email_form" ).submit(function( event ) {
 	event.preventDefault();
-	$("#submit").click();
+	var $emailAddress = $("input[name=email]").val();
+	$.post('foo.html',$emailAddress,function(data,status) {
+	    alert("Data: " + data + "\nStatus: " + status);
+	});
+	if ($emailAddress != 'myname@example.com' && $emailAddress != '') {
+		$( "#form_elements" ).fadeTo( "slow" , 0.0, function() {
+			$( "#thank_you_text" ).fadeTo( "slow" , 1.0, function() {				
+			// alert($emailAddress);
+			});
+		});
+	}
 });
 
 $( "#submit" ).click(function() {
-  $( "#form_elements" ).fadeTo( "slow" , 0.0, function() {
-	  $( "#thank_you_text" ).fadeTo( "slow" , 1.0, function() {
-		var $emailAddress = $("input[name=email]").val();
-		// alert($emailAddress);
-	  });
-  });
+	$("#email_form").submit();
 });
