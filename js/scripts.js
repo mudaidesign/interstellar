072886e626e981ee364f711a8670e5c707b5ed2b
David Vieser <david@vieser.org>
2014-10-30 09:24:03 -0500

    bug fixes, font changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
-   $window = $(window);
- 
-   $("#background").each(function(){
-     var $scroll = $(this);
-                     
-      $(window).scroll(function() {
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-        var coords = '0% '+ yPos + 'px';
-        $scroll.css({ backgroundPosition: coords });    
-      });
-   });
+	$window = $(window);
+	$scroll = $("#background");
+	                 
+	$(window).scroll(function() {
+		var yPos = -($window.scrollTop() / 2); 
+		var coords = '0 '+ yPos + 'px';
+		$scroll.css({ backgroundPosition: coords });
+	});
 });
 
 document.createElement("section");
